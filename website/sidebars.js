f64a51ab116153426acab02095d6861cfb015162
Hugo van Rijswijk <hugo.van.rijswijk@nn.nl>
2020-01-22 11:33:23 +0100

    Update docs

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,8 +6,7 @@
  */
 
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+  docs: {
+    Compass: ['introduction', 'doc1', 'doc2', 'doc3'],
   },
 };
